2635cf6ee2fa898d7b9cb6afc29b7de099fe29e3
Matt Bordas <bordasmatt@gmail.com>
2021-01-15 23:34:22 -0800

    Fixed skills gap

--- src/components/PageFragments/HomePage/SkillProgress.jsx
+++ src/components/PageFragments/HomePage/SkillProgress.jsx
@@ -5,7 +5,7 @@ import ProgressBar from '../../Progress';
 const SkillsProgress = () => (
   <div>
     <h2>My Skills</h2>
-    <Row gutter={[20, 20]}>
+    <Row gutter={[20]}>
       <Col xs={24} sm={24} md={12}>
 
         <ProgressBar
